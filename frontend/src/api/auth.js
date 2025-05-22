ab7a82f71db7a45e9a6966eb9f13cc35809bb0b0
Iván Delgado <ivanwilfrido.delgado.chaparro.alu@iesjulioverne.es>
2025-05-22 19:10:48 +0200

    Borrado 1

--- frontend/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import API from './client'
-
-export const loginUser = async (email, password) => {
-  const response = await API.post('/auth/login', {
-    email,
-    password,
-  })
-  return response.data
-}
